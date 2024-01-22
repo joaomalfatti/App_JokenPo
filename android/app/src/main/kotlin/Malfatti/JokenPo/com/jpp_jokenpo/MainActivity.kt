45dd46709f3f7f41a03928ff24dc0ca2f16e4490
joaomalfatti <joao.cursos1210@gmail.com>
2024-01-22 16:17:49 -0400

    Criação apenas da interface do jogo

---
+++ android/app/src/main/kotlin/Malfatti/JokenPo/com/jpp_jokenpo/MainActivity.kt
@@ -0,0 +1,6 @@
+package Malfatti.JokenPo.com.jpp_jokenpo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Jogo.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+
+class Jogo extends StatefulWidget {
+  const Jogo({super.key});
+
+  @override
+  State<Jogo> createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("JokenPo"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          //1- text
+          //2- imagem
+          //3- text resultado
+          //4- Linha 3 imagens
+          const Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child:
+                Text(
+                  "Escolha do App",
+                  textAlign: TextAlign.center,
+                  style:
+                    TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+              ),
+          ),
+          Image.asset("imagens/padrao.png"),
+          const Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child:
+            Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style:
+              TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row (
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset("imagens/papel.png", height: 95,),
+              Image.asset("imagens/pedra.png", height: 95),
+              Image.asset("imagens/tesoura.png", height: 95),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
